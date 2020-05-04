28830ef5c37b89b1df73aa0ab58f7ffa536741da
0730chen <1252331215@qq.com>
2020-05-04 19:22:06 +0800

    feat：添加使用è¯说明

---
+++ README.md
@@ -0,0 +1,1 @@
+#### typescript koa 脚手架
--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
-const Koa = require('koa')
+import Koa, {BaseContext, Request} from 'koa'
 const app = new Koa()
 
-app.use( async (ctx) => {
+app.use( async ( ctx:BaseContext ) => {
     ctx.body = 'hello koa2'
-})
+});
 
 app.listen(3000)
 console.log('[demo] start-quick is starting at port 3000')
